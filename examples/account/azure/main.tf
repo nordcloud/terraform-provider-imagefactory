62678f273dd1daf97c5fa4d2cd57b8795a370123
Kamil Piotrowski <kamil.piotrowski@nordcloud.com>
2021-12-08 13:45:59 +0100

    [IF-319] Add CI/CD pipeline
    This pipeline builds provider for every OS arch, creates release, and uploads the assets
    
    - Matrix strategy is used to build all binary versions in parallel
    - Generate metadata file for TF Registry
    - Generate SHA256 file for all zips
    - Sign SHA256 file with GPG key
    
    This process is fully compatible with the TF Registry process. If we decide to make it public it should work out of the box. We only need to configure the TF registry, upload the GPG key, and setup webhook.

--- examples/account/azure/main.tf
+++ examples/account/azure/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_version = ">= 0.14"
   required_providers {
     imagefactory = {
-      source  = "nordcloud.com/mc/imagefactory"
+      source  = "nordcloud.com/klarity/imagefactory"
       version = "~> 1.0"
     }
   }

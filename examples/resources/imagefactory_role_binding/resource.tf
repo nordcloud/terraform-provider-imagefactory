1214b95cb959b9274a4276f66f8d6580a27cabd1
Kamil Piotrowski <kamil.piotrowski@nordcloud.com>
2021-12-21 14:16:46 +0100

    Fix role_binding documentation

--- examples/resources/imagefactory_role_binding/resource.tf
+++ examples/resources/imagefactory_role_binding/resource.tf
@@ -1,114 +1,21 @@
-# AWS Template
+// Copyright 2021 Nordcloud Oy or its affiliates. All Rights Reserved.
 
-data "imagefactory_distribution" "ubuntu18" {
-  name           = "Ubuntu Server 18.04 LTS"
-  cloud_provider = "AWS"
+resource "imagefactory_role_binding" "user_binding" {
+  kind = "USER"
+  role = "ADMIN"
+  subject = "user@nordcloud.com"
 }
 
-resource "imagefactory_template" "template" {
-  name            = "Ubuntu1804"
-  description     = "Ubuntu 18.04 on AWS"
-  cloud_provider  = "AWS"
-  distribution_id = data.imagefactory_distribution.ubuntu18.id
-  config {
-    aws {
-      region = "eu-west-1"
-    }
-    notifications {
-      type = "SNS"
-      uri  = "arn:aws:sns:eu-west-1:123456789012:Topic"
-    }
-    tags {
-      key   = "KEY_ONE"
-      value = "VALUE_A"
-    }
-    tags {
-      key   = "KEY_TWO"
-      value = "VALUE_B"
-    }
-  }
+data "imagefactory_api_key" "api_key" {
+  name = "test_api_key"
 }
 
-output "template" {
-  value = imagefactory_template.template
+resource "imagefactory_role_binding" "key_binding" {
+  kind = "API_KEY"
+  role = "READ_ONLY"
+  subject = data.imagefactory_api_key.api_key.id
 }
 
-# AZURE Template
-
-data "imagefactory_distribution" "ubuntu18" {
-  name           = "Ubuntu Server 18.04 LTS"
-  cloud_provider = "AZURE"
-}
-
-resource "imagefactory_component" "build_template" {
-  name            = "Install nginx"
-  description     = "Install nginx on Ubuntu"
-  stage           = "BUILD"
-  cloud_providers = ["AZURE"]
-  os_types        = ["LINUX"]
-  content {
-    script             = <<-EOT
-      apt-get update && apt-get install nginx -y
-    EOT
-    provisioner = "SHELL"
-  }
-}
-
-resource "imagefactory_component" "test_component" {
-  name            = "Test nginx"
-  description     = "Test nginx is installed"
-  stage           = "TEST"
-  cloud_providers = ["AZURE"]
-  os_types        = ["LINUX"]
-  content {
-    script             = <<-EOT
-      ps aux | grep nginx
-      systemctl is-active --quiet nginx || echo "nginx is not running"; exit 1
-    EOT
-    provisioner = "SHELL"
-  }
-}
-
-data "imagefactory_system_component" "hardening-level-1" {
-  name = "Hardening level 1"
-  cloud_provider = "AZURE"
-  stage = "BUILD"
-}
-
-resource "imagefactory_template" "template" {
-  name            = "Ubuntu1804"
-  description     = "Ubuntu 18.04 on Azure"
-  cloud_provider  = "AZURE"
-  distribution_id = data.imagefactory_distribution.ubuntu18.id
-  config {
-    azure {
-      exclude_from_latest = true
-      replica_regions     = ["westeurope"]
-    }
-    build_components {
-      id = data.imagefactory_system_component.hardening-level-1.id
-    }
-    build_components {
-      id = imagefactory_component.build_template.id
-    }
-    test_components {
-      id = imagefactory_component.test_component.id
-    }
-    notifications {
-      type = "WEB_HOOK"
-      uri  = "https://webhook.call.api.address"
-    }
-    tags {
-      key   = "KEY_ONE"
-      value = "VALUE_A"
-    }
-    tags {
-      key   = "KEY_TWO"
-      value = "VALUE_B"
-    }
-  }
-}
-
-output "template" {
-  value = imagefactory_template.template
+output "user_binding" {
+  value = imagefactory_role_binding.user_binding
 }

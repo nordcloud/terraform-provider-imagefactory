6bbade18a2a8927627ed9d459eb296545a3050d2
sternik <sternik@users.noreply.github.com>
2024-02-29 10:18:58 +0000

    Update license headers

--- examples/resources/imagefactory_custom_component/resource.tf
+++ examples/resources/imagefactory_custom_component/resource.tf
@@ -1,4 +1,4 @@
-// Copyright 2021-2023 Nordcloud Oy or its affiliates. All Rights Reserved.
+// Copyright 2021-2024 Nordcloud Oy or its affiliates. All Rights Reserved.
 
 # An example of a SHELL component
 
